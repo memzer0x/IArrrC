263e04a6ac01e237b46d3de3afc2aaab7696d3d6
memzer0x <92188025+memzer0x@users.noreply.github.com>
2022-01-29 01:34:48 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,10 @@
 module.exports = {
-    irc_server: "irc.reversing-ninja.com", // Server to connect to
+    irc_server: "irc.reversing-ninja.com",  // Server to connect to
     irc_channel: ["#pirategod"],            // Channel the bot will automatically join
-    irc_nickname: "IArrrC",            // Nickname of the bot on the IRC Channel
+    irc_nickname: "IArrrC",                 // Nickname of the bot on the IRC Channel
+    irc_realname: "IArrrC by K0vichski",    // Realname of the bot
     max_logs_line: 20000,                   // Save logs to file every 100 lines of text and cleanup array
-    logfile_name: "iaaarc.logs"
+    logfile_name: "iaaarc.logs",            // Log file name
+    autoRejoin: true,                       // Auto Rejoin if the bot lose connection
+    autoRenick: true,                       // Auto Renick if nickname is already token by someone
 }
